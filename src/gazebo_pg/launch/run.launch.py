95b33e58be8e9888a1c0b3aa6190ea847ef46ab4
Hesam Taherzadegan <Taherzadehesam@gmail.com>
2024-09-24 12:02:50 +0330

    Add a custom mesh to the robot urdf - change in cmake - urdf and launch

--- src/gazebo_pg/launch/run.launch.py
+++ src/gazebo_pg/launch/run.launch.py
@@ -1,8 +1,9 @@
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, SetEnvironmentVariable
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+
 
 
 from launch_ros.actions import Node
@@ -17,6 +18,18 @@ def generate_launch_description():
 
     xacro_file = os.path.join(get_package_share_directory(pkg_name),file_subpath)
     robot_description_raw = xacro.process_file(xacro_file).toxml()
+
+
+
+    set_gazebo_model_path = SetEnvironmentVariable(
+        name='GAZEBO_MODEL_PATH',
+        value=os.path.join(get_package_share_directory('gazebo_pg')) + ':' + os.environ.get('GAZEBO_MODEL_PATH', '')
+    )
+
+    set_gazebo_resource_path = SetEnvironmentVariable(
+        name='GAZEBO_RESOURCE_PATH',
+        value=os.path.join(os.path.join(get_package_share_directory('gazebo_pg')), 'materials', 'scripts') + ':' + os.environ.get('GAZEBO_RESOURCE_PATH', '')
+    )
 
     node_robot_state_publisher = Node(
         package='robot_state_publisher',
@@ -57,6 +70,8 @@ def generate_launch_description():
     )
 
     return LaunchDescription([
+        set_gazebo_model_path,
+        set_gazebo_resource_path,
         gazebo,
         node_robot_state_publisher,
         node_joint_state_publisher,
